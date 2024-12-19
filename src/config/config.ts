c61e984d036166a7501f40a81025631ba4b956dc
Sreehari P V <sreehari.valsan@calpinetech.com>
2024-12-19 15:34:56 +0530

    Changed search engine url

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  searxngInstance: "https://search.ascend.de",
+  searxngInstance: "https://avoxfg.tunnel.pyjam.as",
   port: process.env.PORT || 3000,
   maxResults: 5,
   timeout: 10000, // 10 seconds timeout for requests
